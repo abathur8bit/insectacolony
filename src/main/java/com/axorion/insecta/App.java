062080415e23a6767c796c25a8b8c1333df0d57e
Abathur8bit <abathur8bit@8bitcoder.com>
2018-11-27 01:08:29 -0500

    New maven project files and directory structure.

---
+++ src/main/java/com/axorion/insecta/App.java
@@ -0,0 +1,13 @@
+package com.axorion.insecta;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
